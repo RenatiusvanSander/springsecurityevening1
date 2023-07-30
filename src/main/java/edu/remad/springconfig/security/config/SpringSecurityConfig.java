e5ad1969df03e36129d17b67a1349e6ca8554306
Remy Meier <remad@web.de>
2023-07-30 22:02:10 +0200

    master: UserDetailService builds credentials and roles for user and admin without encoding

--- src/main/java/edu/remad/springconfig/security/config/SpringSecurityConfig.java
+++ src/main/java/edu/remad/springconfig/security/config/SpringSecurityConfig.java
@@ -1,19 +1,46 @@
 package edu.remad.springconfig.security.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SpringSecurityConfig {
+	
+	@Value("${spring.websecurity.debug:true}")
+    boolean webSecurityDebug;
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.debug(webSecurityDebug);
+    }
+    
+    /**
+     * Users stores credentials and roles in memory of server.
+     * Reorganize into own service class!
+     * 
+     * @return service tells user details
+     */
+    @Bean
+    public UserDetailsService users() {
+    	UserDetails user = User.builder()
+    		.username("user")
+    		.password("dummy")
+    		.roles("USER")
+    		.build();
+    	UserDetails admin = User.builder()
+    		.username("admin")
+    		.password("dummyAdmin")
+    		.roles("USER", "ADMIN")
+    		.build();
+    	
+    	return new InMemoryUserDetailsManager(user, admin);
+    }
 }
