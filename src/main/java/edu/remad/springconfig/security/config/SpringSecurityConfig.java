bba4170052a153480af6747e1b5f6b40b0613696
Remy Meier <remad@web.de>
2023-08-06 00:30:16 +0200

    master: standard password encoder initialized via bean passwordEncoder

--- src/main/java/edu/remad/springconfig/security/config/SpringSecurityConfig.java
+++ src/main/java/edu/remad/springconfig/security/config/SpringSecurityConfig.java
@@ -1,5 +1,8 @@
 package edu.remad.springconfig.security.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -36,15 +41,25 @@ public class SpringSecurityConfig {
 			
 			UserDetails user = User.builder()
 				.username("user")
-				.password(encoder.encode("dummy"))
+				.password("{bcrypt}$2a$10$kLtNvCSnOilfMnmIt8mtI.D0dp231FuXA1VpNvjqxGiU1NVZ.oh1C")
 				.roles("USER")
 				.build();
 			UserDetails admin = User.builder()
 				.username("admin")
-				.password(encoder.encode("adminDummy"))
+				.password("{bcrypt$2a$10$akAbLTt.i2mgafoQiilXsOfch7KtbvKrEO63xdefM/6qI8f/wTp5S")
 				.roles("USER", "ADMIN")
 				.build();
 			
 			return new InMemoryUserDetailsManager(user, admin);
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+    	String idForEncode = "bcrypt";
+    	Map<String, PasswordEncoder> encoders = new HashMap<>();
+    	encoders.put(idForEncode, new BCryptPasswordEncoder());
+
+    	return
+    	    new DelegatingPasswordEncoder(idForEncode, encoders);
     }
 }
