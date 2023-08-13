d5376d403b2ca403fdc9a0614aee6dd54623bee0
Remy Meier <remad@web.de>
2023-08-13 23:51:59 +0200

    feature/LogoutHandler: own logouthandler on progress

--- src/main/java/edu/remad/springconfig/security/logout/WebLogoutHandler.java
+++ src/main/java/edu/remad/springconfig/security/logout/WebLogoutHandler.java
@@ -8,8 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
+import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
 
 public class WebLogoutHandler implements LogoutHandler{
 	
@@ -18,8 +22,16 @@ public class WebLogoutHandler implements LogoutHandler{
 	@Override
 	public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
 		cookies = Arrays.asList(request.getCookies()); // cookies to clear
-
+		SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder.getContextHolderStrategy();
+		
+		SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+		securityContextLogoutHandler.setClearAuthentication(true);
+		securityContextLogoutHandler.setInvalidateHttpSession(true);
+		securityContextLogoutHandler.setSecurityContextHolderStrategy(securityContextHolderStrategy);
+		securityContextLogoutHandler.logout(request, response, authentication);
+		
 		ClearSiteDataHeaderWriter clearSiteData = new ClearSiteDataHeaderWriter();
+		clearSiteData.writeHeaders(request, response);
 	}
 
 }
