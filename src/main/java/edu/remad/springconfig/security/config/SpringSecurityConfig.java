3133e2f64ce8696297f43efbdb7cdb9800dc94c8
Remy Meier <remad@web.de>
2023-08-17 22:49:05 +0200

    Revert "forkedMaster/JDBCAuthentication: JDBC Authentication is now inserted in"
    This reverts commit 823359050f0a9c35e8052b56428249685ef77b88.

--- src/main/java/edu/remad/springconfig/security/config/SpringSecurityConfig.java
+++ src/main/java/edu/remad/springconfig/security/config/SpringSecurityConfig.java
@@ -6,12 +6,8 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -35,15 +31,5 @@ public class SpringSecurityConfig {
 		encoders.put(idForEncode, new BCryptPasswordEncoder());
 
 		return new DelegatingPasswordEncoder(idForEncode, encoders);
-	}
-	
-	@Bean
-	public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) 
-	  throws Exception {
-	    return http.getSharedObject(AuthenticationManagerBuilder.class)
-	      .userDetailsService(userDetailsService)
-	      .passwordEncoder(passwordEncoder)
-	      .and()
-	      .build();
 	}
 }
