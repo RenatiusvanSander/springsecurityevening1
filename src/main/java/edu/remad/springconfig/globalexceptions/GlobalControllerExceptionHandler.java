4606242ef30ce63199b8b544e2a873915940574d
RenatiusvanSander / R. Meier <remad@web.de>
2023-09-27 22:44:04 +0200

    mster: globalexception handler started

---
+++ src/main/java/edu/remad/springconfig/globalexceptions/GlobalControllerExceptionHandler.java
@@ -0,0 +1,22 @@
+package edu.remad.springconfig.globalexceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+	
+	@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Data integrity violation")
+	// 409
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ModelAndView conflicht(HttpServletRequest req, Exception exception)
+			throws Exception {
+		return new ModelAndView();
+	}
+}
