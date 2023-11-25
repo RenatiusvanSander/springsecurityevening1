b31c648ea9b22fc7ad4c62e8a19e522eac9e8582
Remy Meier <remad@web.de>
2023-11-25 01:38:43 +0100

    master: caching resources. gzip use when browser shall get an answer

--- src/main/java/edu/remad/springconfig/mvcconfig/WebMvcConfig.java
+++ src/main/java/edu/remad/springconfig/mvcconfig/WebMvcConfig.java
@@ -17,6 +17,9 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.resource.EncodedResourceResolver;
+import org.springframework.web.servlet.resource.GzipResourceResolver;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
@@ -48,6 +51,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/static-resources/**").addResourceLocations("/static-resources")
+				.setCachePeriod(365 * 24 * 60 * 60).resourceChain(true).addResolver(new EncodedResourceResolver())
+				.addResolver(new PathResourceResolver());
 	}
 
 	@Override
@@ -55,72 +61,73 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		// TODO registry.addInterceptor(new LoggerInterceptor());
 		// TODO registry.addInterceptor(new UserInterceptor());
 		// TODO registry.addInterceptor(new SessionTimerInterceptor());
-		registry.addInterceptor(new HandlerTimeLoggingInterceptor()).addPathPatterns("/**").excludePathPatterns("/secure/**");
+		registry.addInterceptor(new HandlerTimeLoggingInterceptor()).addPathPatterns("/**")
+				.excludePathPatterns("/secure/**");
 		registry.addInterceptor(new SecuritySignupFilter()).addPathPatterns("/process-signup");
 		registry.addInterceptor(localeChangeInterceptor());
 	}
-	
+
 	@Bean
 	public SystemEnvironment systemEnvironment() {
 		return SystemEnvironmentFactory.getInstance();
 	}
-	
+
 	@Bean
 	public JavaMailSender createJavaMailSender(SystemEnvironment systemEnvironment) {
-	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-	    mailSender.setHost("smtp.web.de");
-	    mailSender.setPort(587);	    
-	    mailSender.setUsername(systemEnvironment.getSmtpUsername());
-	    mailSender.setPassword(systemEnvironment.getSmtpPassword());
-	    
-	    Properties props = mailSender.getJavaMailProperties();
-	    props.put("mail.transport.protocol", "smtp");
-	    props.put("mail.smtp.auth", "true");
-	    props.put("mail.smtp.starttls.enable", "true");
-	    props.put("mail.properties.mail.smtp.starttls.required","true");
-	    props.put("mail.properties.mail.smtp.ssl.enable","false");
-	    props.put("mail.properties.mail.smtp.timeout", "15000");
-	    props.put("mail.properties.mail.smtp.connectiontimeout", "15000");
-	    props.put("mail.properties.mail.smtp.socketFactory.fallback", "true");
-	    props.put("mail.debug", "true");
-	    
-	    return mailSender;
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.web.de");
+		mailSender.setPort(587);
+		mailSender.setUsername(systemEnvironment.getSmtpUsername());
+		mailSender.setPassword(systemEnvironment.getSmtpPassword());
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.properties.mail.smtp.starttls.required", "true");
+		props.put("mail.properties.mail.smtp.ssl.enable", "false");
+		props.put("mail.properties.mail.smtp.timeout", "15000");
+		props.put("mail.properties.mail.smtp.connectiontimeout", "15000");
+		props.put("mail.properties.mail.smtp.socketFactory.fallback", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
 	}
-	
+
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.freeMarker().cache(true).cacheLimit(1000);
 	}
-	
+
 	@Bean
 	public FreeMarkerConfigurer freeMarkerConfigurer() {
 		Properties properties = new Properties();
 		properties.put("auto_import", "/spring.ftl as spring");
-		
+
 		FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
 		configurer.setTemplateLoaderPath("/WEB-INF/templates/freemarker");
 		configurer.setDefaultEncoding(StandardCharsets.UTF_8.displayName());
 		configurer.setFreemarkerSettings(properties);
-		
+
 		return configurer;
 	}
-	
-	@Bean(name="messageSource")
+
+	@Bean(name = "messageSource")
 	public ResourceBundleMessageSource messageSource() {
 		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.addBasenames("messages/messages","messages/welcomes");
+		messageSource.addBasenames("messages/messages", "messages/welcomes");
 		messageSource.setDefaultEncoding("UTF-8");
-		
+
 		return messageSource;
 	}
-	
+
 	@Bean
 	public LocaleChangeInterceptor localeChangeInterceptor() {
 		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-		
+
 		return localeChangeInterceptor;
 	}
-	
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/cors/greeting-javaconfig").allowedOrigins("http://localhost:8080");
