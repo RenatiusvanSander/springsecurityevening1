5f1a8b3ef7e99ceccb6de62acab1382d214fc05d
Remy Meier <remad@web.de>
2023-12-10 18:38:15 +0100

    master: scoped beans example

--- src/main/java/edu/remad/springconfig/controllers/MainController.java
+++ src/main/java/edu/remad/springconfig/controllers/MainController.java
@@ -1,15 +1,24 @@
 package edu.remad.springconfig.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import edu.remad.springconfig.scopes.HitCounter;
+
 @Controller
 @RequestMapping("/home")
 public class MainController {
+	
+	@Autowired
+	private HitCounter hitCounter;
 
 	@GetMapping("/")
 	public String getHome() {
+		hitCounter.setHits(hitCounter.getHits() + 1);
+		System.out.println("The number of hits is : " + hitCounter.getHits());
+		
 		return "home";
 	}
 }
